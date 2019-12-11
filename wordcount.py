801afd8d33b0555711bc8489a6c579f90917cbc2
GopinathMC <58396858+GopinathMC@users.noreply.github.com>
2019-12-11 14:52:27 +0530

    Add files via upload

---
+++ wordcount.py
@@ -0,0 +1,26 @@
+#simple wordcount problem using pyspark and register it as dataFrame
+
+import sys
+from pyspark.sql import SQLContext
+sqlContext = SQLContext(sc)
+
+words = sc.textFile("file:///efs/home/ps900191/test2")
+
+lines = words.flatMap(lambda line : line.split(" "))
+
+wordcount = lines.map(lambda word : (word,1))
+
+aggrcount = wordcount.reduceByKey(lambda a,b : a+b)
+
+df1 = sqlContext.createDataFrame(aggrcount)
+
+print("Toatl words in the files are {}".format(df1.count()))
+print("Here is the words and its counts!!")
+
+df1.show()
+
+
+
+
+
+
