217a6440321c03d264de94a960b50653970f4872
GopinathMC <58396858+GopinathMC@users.noreply.github.com>
2020-05-24 22:13:13 +0530

    Update windowsHive.sql

--- windowsHive.sql
+++ windowsHive.sql
@@ -10,26 +10,26 @@ over(partition by stockcode order by invoiceno
 rows between unbounded preceding and current row)cumulative_avg
 from retail_price;
 
---to get the minimum and maximum amoun spent for particular invoice number and its details
+--to get the minimum and maximum amount spent for particular invoice number and its details
 with detail as (
+select * from(
 select invoiceno,stockcode,customerid,unitprice,
 min(unitprice) over(partition by invoiceno order by invoiceno)min_price,
 max(unitprice) over(partition by invoiceno order by invoiceno)max_price,
 row_number() over (partition by invoiceno order by invoiceno) as r1
-from retail_price)
+from retail_price)a where a.r1=1 )
 
-
-select invoiceno,stockcode,customerid,unitprice,'minimum' as less_high,
+select invoiceno,stockcode,customerid,unitprice,'minimum' as less_high
 from retail_price
-and concat(cast(invoiceno as string),cast(unitprice as string)) in 
-(select concat(cast(invoiceno as string),cast(min_price as string)) from detail
-where r1=1)
+where concat(cast(invoiceno as string),cast(unitprice as string)) in 
+(select concat(cast(invoiceno as string),cast(min_price as string)) from detail)
 
 union all
 
-select invoiceno,stockcode,customerid,unitprice,'maximum' as less_high,
-row_number() over(partition by invoiceno,stockcode,customerid order by invoiceno) as r2
+select invoiceno,stockcode,customerid,unitprice,'maximum' as less_high
 from retail_price
 where concat(cast(invoiceno as string),cast(unitprice as string)) in 
-(select concat(cast(invoiceno as string),cast(max_price as string)) from detail
-where r1=1)
+(select concat(cast(invoiceno as string),cast(max_price as string)) from detail)
+
+
+
