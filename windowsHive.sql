7817c87adddd2fe9bd92ff39ba43ad9ce8ee0185
GopinathMC <58396858+GopinathMC@users.noreply.github.com>
2020-05-24 20:32:19 +0530

    Update windowsHive.sql

--- windowsHive.sql
+++ windowsHive.sql
@@ -1,6 +1,11 @@
 --this below code will perform cumulative sum of unit price based on stockcode, if stockcode reappera those sum will get added up
-
 select invoiceno,stockcode,customerid,unitprice,sum(unitprice) 
 over(partition by stockcode order by invoiceno 
 rows between unbounded preceding and current row)cumulative_sum
+from retail_price;
+
+--this is for cumulative average
+select invoiceno,stockcode,customerid,unitprice,avg(unitprice) 
+over(partition by stockcode order by invoiceno 
+rows between unbounded preceding and current row)cumulative_avg
 from retail_price;
