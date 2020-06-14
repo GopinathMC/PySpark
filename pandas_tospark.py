9e7bff036abffcc8ba4b0eee2f18ac136916282b
GopinathMC <58396858+GopinathMC@users.noreply.github.com>
2020-06-14 22:26:16 +0530

    Update pandas_tospark.py

--- pandas_tospark.py
+++ pandas_tospark.py
@@ -6,11 +6,11 @@ from impala.dbapi import connect
 from impala.util import as_pandas
 IMPALA_HOST = "peanut-impala.cargill.com"
 IMPALA_PORT = 21050
-IMPALA_USER = "ps329181@AP.CORP.CARGILL.COM"
-IMPALA_PASSWORD = "Welc0me@1"
+IMPALA_USER = "ps462914@NA.CORP.CARGILL.COM"
+IMPALA_PASSWORD = "6~[[Op]z[U"
 conn = connect(host=IMPALA_HOST,port=IMPALA_PORT,use_ssl=True,ca_cert=None,auth_mechanism='PLAIN',user=IMPALA_USER,password=IMPALA_PASSWORD)
 cursor = conn.cursor()
-cursor.execute('Select * from prd_product_china_anf.i_submission_schedule;')
+cursor.execute('select * from prd_internal_ibp_interim.md_uom;')
 df_pd = as_pandas(cursor)
 
 spark = SparkSession.builder.appName('pandasToSparkDF').getOrCreate()
