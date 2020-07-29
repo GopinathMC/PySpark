66057b2f42d54ab933e6efb1f78ce2fc9a649442
GopinathMC <58396858+GopinathMC@users.noreply.github.com>
2020-07-29 19:26:16 +0530

    Update pandasUDF.py

--- pandasUDF.py
+++ pandasUDF.py
@@ -3,13 +3,10 @@
 #1. Series to Series(pd.Series -> pd.Series)
 import pandas as pd
 from pyspark.sql.functions import pandas_udf
-
 df=spark.range(10)
-
 @pandas_udf('long')
 def plus_one(s: pd.Series) -> pd.Series:
     return s+1
-    
 df_1 = sf.withColumn('id+1',plus_one('id'))
 
 #2. Iterator series to Iterator series(Iterator[pd.Series] -> Iterator[pd.Series] -> Iterator[pd.Series])
@@ -18,3 +15,11 @@ from typing import Iterator
 @pandas_udf('long')
 def plus_one(iterator: Iterator[pd.Series]) -> Iterator[pd.Series]:
     return map(lambda s : s+1,iterator)
+
+#3. Multiple Iterator Series to Iterator Series((Iterator[Tuple[pd.Series,pd.Series]] -> Iterator[pd.Series] -> Iterator[pd.Series]))
+#takes multiple cols,length of input and output should same
+from typing import Iterator,Tuple
+@pandas_udf('long')
+def doubleIT(iterator: Iterator[Tuples[pd.Series,pd.Series]]) -> Iterator[pd.Series]:
+    return (a*b for a,b in iterator)
+df_2 = df.withColumn('doubled',doubleIT('id','id'))
