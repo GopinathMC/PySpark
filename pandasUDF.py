d82d9538239e085f8f001864d77f25c572bcb40f
GopinathMC <58396858+GopinathMC@users.noreply.github.com>
2020-07-29 19:16:47 +0530

    Update pandasUDF.py

--- pandasUDF.py
+++ pandasUDF.py
@@ -1,6 +1,6 @@
 #Types:
-#1. Series to Series
 
+#1. Series to Series(pd.Series -> pd.Series)
 import pandas as pd
 from pyspark.sql.functions import pandas_udf
 
@@ -11,3 +11,10 @@ def plus_one(s: pd.Series) -> pd.Series:
     return s+1
     
 df_1 = sf.withColumn('id+1',plus_one('id'))
+
+#2. Iterator series to Iterator series(Iterator[pd.Series] -> Iterator[pd.Series] -> Iterator[pd.Series])
+#takes singles column, lenghth of input and output should be same
+from typing import Iterator
+@pandas_udf('long')
+def plus_one(iterator: Iterator[pd.Series]) -> Iterator[pd.Series]:
+    return map(lambda s : s+1,iterator)
